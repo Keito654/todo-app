1898a6a0770c21283131d299aca6b0c1421b9cac
Keito654 <keito19981129@gmail.com>
2024-04-14 23:36:05 +0900

    lintの追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import antfu from '@antfu/eslint-config';
+import { configs as ReactQueryConfigs, rules as ReactQueryRules } from '@tanstack/eslint-plugin-query';
 
 export default antfu({
   stylistic: {
@@ -10,4 +11,16 @@ export default antfu({
     'tsconfig.node.json',
     'vite.config.ts',
   ],
-});
+}).append(
+  {
+    plugins: {
+      '@tanstack/eslint-plugin-query': { rules: ReactQueryRules, configs: ReactQueryConfigs },
+    },
+    rules: {
+      '@tanstack/eslint-plugin-query/exhaustive-deps': 'error',
+      '@tanstack/eslint-plugin-query/no-rest-destructuring': 'warn',
+      '@tanstack/eslint-plugin-query/stable-query-client': 'error',
+    },
+
+  },
+);
